5042a29470b431a3f3a3da022354c9a78299f6a3
Saul <saulb3564@gmail.com>
2022-10-09 23:08:41 -0500

    Se agregaron las figuras que forman al ahorcado, el mesaje cuando el juego termina, es decir, cuando la figura esta completa y se agregaron las coordenadas donde se dibujaran las figuras.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,100 @@ const pincel = lienzo.getContext('2d')
 let listaPalabras = ["SABATINO", "BIGPINK", "TECLADO", "PINTURA"]
 let palabra = undefined
 let finDelJuego = false
+let error = 0
+
+//Figuras que hacen al ahorcado.
+let valoresLinea = [
+    //Poste o linea en vertical.
+	{
+		error: 1,
+		moveToX: 50,
+		moveToY: 340,
+		lineToX: 50, 
+		lineToY: 20
+	},
+
+	//Linea en horizontal pegada al poste.
+	{
+		error: 2,
+		moveToX: 50,
+		moveToY: 20,
+		lineToX: 200, 
+		lineToY: 20
+	},
+	
+    //Linea en diagonal formando un triangulo con el poste y la horizontal.
+	{
+		error: 3,
+		moveToX: 50,
+		moveToY: 50,
+		lineToX: 80, 
+		lineToY: 20
+	},
+	
+    //Cuerda que cuelga de la linea horizontal.
+	{
+		error: 4,
+		moveToX: 200,
+		moveToY: 20,
+		lineToX: 200,
+		lineToY: 70
+	},
+	
+    //Cabeza del personaje.
+	{
+		error: 5,
+		arcX: 200,
+		arcY: 90,
+		radio: 20,
+		angulo: 0
+	},
+	
+    //Tronco del personaje.
+	{
+		error: 6,
+		moveToX: 200,
+		moveToY: 110,
+		lineToX: 200,
+		lineToY: 180
+	},
+	
+    //Brazo derecho del personaje.
+	{
+		error: 7,
+		moveToX: 200,
+		moveToY: 120,
+		lineToX: 220,
+		lineToY: 150
+	},
+	
+    //Brazo izquierdo del personaje.
+	{
+		error: 8,
+		moveToX: 200,
+		moveToY: 120,
+		lineToX: 180,
+		lineToY: 150
+	},
+	
+    //Pierna derecha del personaje.
+	{
+		error: 9,
+		moveToX: 200,
+		moveToY: 180,
+		lineToX: 180,
+		lineToY: 220
+	},
+	
+    //Pierna izquierda del personaje.
+	{
+		error: 10,
+		moveToX: 200,
+		moveToY: 180,
+		lineToX: 220,
+		lineToY: 220
+	}
+]
 
 window.addEventListener("load", elegirPalabra)
 
@@ -28,6 +122,7 @@ function dibujarBaseCanvas() {
 /* 
 * Genera un numero aleatorio para determinar la posicion de la palabra que se utilizara.
 * Ejecuta la funcion generadora de TextArea proporcional a la longitud de la palabra.
+* Ejecuta la funcion que dibuja la base del ahorcado.
 */
 function elegirPalabra() {
     const posicion = Math.floor((Math.random() * ((listaPalabras.length - 1) + 1)))
@@ -63,6 +158,12 @@ function generarTextArea() {
 * No utiliza caracteres especiales, puntos, numeros, etc. Solo letras.
 * Llama a una funcion para determinar la posicion de las letras en la palabra.
 * Llama a una funcion para escribir en los textArea.
+* Cuando se preciona una tecla que no coincide con letras de la palabra,
+  el contador 'error' se incrementa en 1.
+* Cada que se comete un error, se llama una funcion que completa la figura,
+  del ahorcado.
+* Cuando se alcanza el limite de errores, termina el juego y lanza un mensaje,
+  de 'Game Over!!' y remueve el evento 'keyup'.
 */
 function obtenerTecla(tecla) {
     const regEx = /[A-Z]\b/
@@ -72,11 +173,45 @@ function obtenerTecla(tecla) {
         if(palabra.includes(tecla.key)) {
             posicion = buscarLetras(palabra, tecla.key)
             escribirTextArea(tecla.key, posicion)
-        } else { 
-            console.log(tecla.key)
+        } else {
+            error += 1
+            mostrarFiguras()
+            if(error === 10) {
+                error = 0
+                finDelJuego = true
+                console.log("Game over!!.")
+                document.removeEventListener('keyup', obtenerTecla, true)
+            }
         }
     }
     verificarTextArea(palabra)
+}
+
+/* 
+* Cada que el usuario comete un error al tratar de adivinar la palabra,
+  se completa la figura del ahorcado.
+* Hay una excepcion al tratar de dibujar la cabeza del personaje y es al cometer,
+  el error numero 5.
+*/
+function mostrarFiguras() {
+	
+	valoresLinea.forEach(element => {
+		if(error === element.error && error !== 5) {
+			pincel.beginPath()
+			pincel.lineWidth = 3
+			pincel.strokeStyle = "black"
+			pincel.moveTo(element.moveToX, element.moveToY)
+			pincel.lineTo(element.lineToX, element.lineToY)
+			pincel.lineCap = "round"
+			pincel.stroke()
+		} else if(error === 5) {
+			pincel.beginPath()
+			pincel.lineWidth = 3
+			pincel.strikeStyle = "black"
+			pincel.arc(element.arcX, element.arcY, element.radio, element.angulo, 2*Math.PI)
+			pincel.stroke()
+		}
+	})
 }
 
 /* 
